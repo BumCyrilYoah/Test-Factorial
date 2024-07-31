ee33e69e018ffc50c26dacffdbdebd9d95172044
BumCyrilYoah <yoahcyril8@gmail.com>
2024-07-31 17:59:04 +0100

    message hello

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
+
+class salary{
+private:
+    int es;
+    friend int printsalary(salary s);
+    };
+    int printsalary(salary s){
+    s.es = s.es+1;
+    }
 int main()
 {
+    salary s;
+    cout<<"\t\t\t\t\t salary is = ";
+    cout<<printsalary(s)<<endl;
 int num = 1;
 int i = 2;
 long long  factorial;
